3f595bd53fc55cc3babd7fd29d0df8b9651954f1
higorvoltz <higor.amorim@voltzmotors.com>
2023-05-10 09:09:25 -0300

    initial

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+menu1 = SecretMenuItem.create(menu_name: "Veggie", restaurant_name: "McDonald's", menu_description: "Veggie")
+menu2 = SecretMenuItem.create(menu_name: "Chicken", restaurant_name: "McDonald's", menu_description: "Chicken")
+menu3 = SecretMenuItem.create(menu_name: "Beef", restaurant_name: "McDonald's", menu_description: "Beef")
